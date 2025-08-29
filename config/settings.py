4e97ad926e9d9696e6e9097e77ede76db72c2d3a
00-hidan-00 <david.taranov@gmail.com>
2025-08-29 22:30:51 +0300

    ✨ Creating, editing, and deleting Articles

--- config/settings.py
+++ config/settings.py
@@ -114,6 +114,7 @@ AUTH_USER_MODEL = "users.User"
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [APPS_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
