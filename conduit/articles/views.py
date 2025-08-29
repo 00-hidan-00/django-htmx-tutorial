4965e72c7bdd24d57543c063cd9984f4efa859ac
00-hidan-00 <david.taranov@gmail.com>
2025-08-29 19:31:38 +0300

    ✨ Home page

--- conduit/articles/views.py
+++ conduit/articles/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.views.generic import ListView
+
+from .models import Article
+
+
+class Home(ListView):
+    """View all published articles for the global feed."""
+
+    template_name = "home.html"
+    queryset = Article.objects.order_by("-created_at")
+    context_object_name = "articles"
